5c4c3283a756ba17d6a574bc846f8ab7092b7ed4
Gazi Ashiq <GaziAshiq.dev@gmail.com>
2020-07-07 00:27:19 +0600

    Update Marooned urls. Remove if.debug from media url

--- Marooned/urls.py
+++ Marooned/urls.py
@@ -24,6 +24,4 @@ urlpatterns = [
                   path('news/', include('news.urls')),
                   path('summernote/', include('django_summernote.urls')),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
